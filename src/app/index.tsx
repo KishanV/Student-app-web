ccd12711b81e112137d0f159ad691028d2f62d70
kishan devani <kishananimation@gmail.com>
2019-11-27 20:57:08 +0530

    Initial App class is written render to body.

---
+++ src/app/index.tsx
@@ -0,0 +1,27 @@
+import React = require("react");
+import './index.scss';
+import {hot} from 'react-hot-loader/root';
+
+interface State {
+
+}
+
+export class App extends React.Component<any, State> {
+    state: State = {};
+
+    constructor(props: any) {
+        super(props);
+    }
+
+    async loadData() {
+
+    }
+
+    render() {
+        return <div className={'App'}>
+
+        </div>;
+    }
+}
+
+export default hot(App);
